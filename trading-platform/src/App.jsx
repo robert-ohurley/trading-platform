f8263f6518977b2eb25022ae7546a7569a5b1a12
Rojet Savage <robert-hurley@hotmail.com.au>
2023-08-12 08:48:25 +1000

    trading platform initial commit

---
+++ trading-platform/src/App.jsx
@@ -0,0 +1,14 @@
+import CryptoTable from "./CryptoTable"
+export default function App() {
+
+
+
+	return (
+		<>
+			<CryptoTable />
+			<h1>
+				Defi
+			</h1>
+		</>
+	)
+}
---
+++ trading-platform/src/CryptoTable.jsx
@@ -0,0 +1,33 @@
+import getCrypto from "./hooks/getCrypto"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+let API_KEY = "84876368-dc36-4e92-b725-4473c646264f";
+
+export default function CryptoTable() {
+	const [cryptoListings, setCryptoListings] = useState([]);
+
+	async function callApi() {
+		let response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD', {
+			headers: {
+			'X-CMC_PRO_API_KEY': API_KEY,
+			'Access-Control-Allow-Origin':true
+			},
+		});
+		
+		const json = response.data;
+		console.log(json);
+		return json;
+	}
+
+	useEffect(() => {
+		let cryptoListings = callApi()
+		setCryptoListings(cryptoListings)
+	}, [])
+
+	return (
+		<div>
+			{cryptoListings}
+		</div>
+	)
+}
