f9f9f7cfabd9c39b78d5056292e2ba6199f42fe9
RojetSavage <robert-hurley@hotmail.com.au>
2023-10-11 17:00:59 +1100

    the final push

--- trading platform/solidity/trade.js
+++ trading platform/solidity/trade.js
@@ -1,4 +1,4 @@
-
+const PRIVATE_KEY = '0x0ce62120e6588ceb45d4b3ee9b551a991d7bde69718b726746bb6b35b0c6db83'; 
 import Web3 from 'web3';
 
 export default async function tradeNft(toAddress, ethValue, nftName, image,) {
@@ -12,7 +12,7 @@ export default async function tradeNft(toAddress, ethValue, nftName, image,) {
 			to: toAddress,
 			value: value,
 			gas: 2000000
-		}, '0x0ce62120e6588ceb45d4b3ee9b551a991d7bde69718b726746bb6b35b0c6db83')
+		}, PRIVATE_KEY)
 
 		let receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
 		receipt.nftName = nftName;
